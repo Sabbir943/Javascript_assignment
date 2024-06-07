ca17c91b0c1627aa85e68598893e0ca7c2fb6ecd
sabbir--ahmad <mdsabbirahmad943@gmail.com>
2024-06-07 09:01:24 +0600

    Javascrict_assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+var name;
+var age;
+name="Sufia<br/>";
+age=23;
+document.write(name);
+document.write(age);
+var num=12;
+console.log(typeof(num));
+var num="123";
+console.log(typeof(num));
+num=parseInt(num);
+console.log(typeof(num));
+console.log(Number("123"))
+var fristName="sabbir";
+var LastName="Ahmad<br/>";
+var fullName= fristName + LastName;
+document.write(fullName);
+var text=prompt("Enter your Country name:");
+document.write("Number of character "+ text.length);
+var text="southAfrica<br/>";
+text=text.charAt(3);
+var text="Banladesh";
+document.write(text);
+text=text.toUpperCase(text);
+document.write(next);
---
+++ upperandlower.js
@@ -0,0 +1,6 @@
+var text="Austorila<br/>";
+text=text.toUpperCase(text);
+document.write(text);
+var text1="Bangladseh";
+text1=text1.toLowerCase(text1);
+document.write(text1);
